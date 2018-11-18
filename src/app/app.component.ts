40170f2325e51a65055777628995b01701591ac7
azran4u <azran4u@gmail.com>
2018-11-18 02:34:39 +0200

    marker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MapMouseEvent, Map } from 'mapbox-gl';
 
 @Component({
   selector: 'my-app',
@@ -7,7 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+  pitch = 0;  
+
+  map: Map;
+  cursorStyle: string;
+
+  center = [-90.96, -0.47];
+
+  geometries = [
+    {
+      'type': 'Point',
+      'coordinates': [
+        -91.395263671875,
+        -0.9145729757782163
+
+      ]
+    },
+    {
+      'type': 'Point',
+      'coordinates': [
+        -90.32958984375,
+        -0.6344474832838974
+      ]
+    },
+    {
+      'type': 'Point',
+      'coordinates': [
+        -91.34033203125,
+        0.01647949196029245
+      ]
+    }
+  ];
+
+  centerMapTo(evt: MapMouseEvent) {
+    this.center = (<any>evt).features[0].geometry.coordinates;
   }
 
+  ngOnInit(){
+    
+  }
 }
