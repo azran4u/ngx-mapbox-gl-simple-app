9061826cb9baea131c4f8f224741cee309a1812d
azran4u <azran4u@gmail.com>
2018-11-21 14:56:14 +0200

    hike

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { MapMouseEvent, Map, Layer, Marker, GeoJSONSource, LngLatLike, LngLat, G
 import { GeoJson, FeatureCollection } from './map';
 import { Geometry, geometry, Point } from '@turf/helpers';
 import {Museums} from '../data/museums';
+import * as data from '../data/hike.geojson';
 
 @Component({
   selector: 'my-app',
@@ -157,4 +158,42 @@ export class AppComponent implements OnInit {
   museumsLayerHide(){
     this.map.setLayoutProperty('museums', 'visibility', 'none');
   }
+
+  // movingElement(){
+  //   var coordinates = data.features[0].geometry.coordinates;
+
+  //       // start by showing just the first coordinate
+  //       data.features[0].geometry.coordinates = [coordinates[0]];
+
+  //       // add it to the map
+  //       map.addSource('trace', { type: 'geojson', data: data });
+  //       map.addLayer({
+  //           "id": "trace",
+  //           "type": "line",
+  //           "source": "trace",
+  //           "paint": {
+  //               "line-color": "yellow",
+  //               "line-opacity": 0.75,
+  //               "line-width": 5
+  //           }
+  //       });
+
+  //       // setup the viewport
+  //       map.jumpTo({ 'center': coordinates[0], 'zoom': 14 });
+  //       map.setPitch(30);
+
+  //       // on a regular basis, add more coordinates from the saved list and update the map
+  //       var i = 0;
+  //       var timer = window.setInterval(function() {
+  //           if (i < coordinates.length) {
+  //               data.features[0].geometry.coordinates.push(coordinates[i]);
+  //               map.getSource('trace').setData(data);
+  //               map.panTo(coordinates[i]);
+  //               i++;
+  //           } else {
+  //               window.clearInterval(timer);
+  //           }
+  //       }, 10);
+  // }
+
 }
